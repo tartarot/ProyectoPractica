b5dddb6622f55e12952ebb7deac9dd7fd8238bf3
tartarot <Eduardslazar@gmail.com>
2020-07-06 00:14:36 -0500

    Updates

---
+++ precache-manifest.6a649fe82a84d5e04b244b8d7c3b89c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb76dd646521ae23f8dad747f9e478d",
+    "url": "/ProyectoPractica/index.html"
+  },
+  {
+    "revision": "40720568243460ab0347",
+    "url": "/ProyectoPractica/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d74d08b1bf9bc340000",
+    "url": "/ProyectoPractica/static/js/2.e99c2b49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ProyectoPractica/static/js/2.e99c2b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40720568243460ab0347",
+    "url": "/ProyectoPractica/static/js/main.bc0f0d9c.chunk.js"
+  },
+  {
+    "revision": "a4785984843439bc1323",
+    "url": "/ProyectoPractica/static/js/runtime-main.e3a1795a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ProyectoPractica/static/media/logo.5d5d9eef.svg"
+  }
+]);
